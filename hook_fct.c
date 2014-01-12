78f39d41701672406744adc51da93d4e149526fb
Moss-id <id.moss@hotmail.fr>
2014-01-12 14:43:33 +0100

    Add color define.

--- hook_fct.c
+++ hook_fct.c
@@ -30,6 +30,6 @@ int		ft_key_hook(int keycode, t_win *window)
 int		ft_expose_hook(t_win *window)
 {
 	/* On affiche l'img */
-	mlx_put_image_to_window(window->mlx, window->win, window->img->img, 0, 0);
+	ft_draw(window, 500, 500);
 	return (0);
 }
